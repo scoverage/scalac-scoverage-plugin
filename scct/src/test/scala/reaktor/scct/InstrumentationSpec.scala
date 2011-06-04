be1d5eb1ae5e16fa6142adb95b029e5acb8cf494
Mikko Koponen <mikko.koponen@reaktor.fi>
2011-06-04 13:04:18 +0300

    Set scala to 2.9

--- scct/src/test/scala/reaktor/scct/InstrumentationSpec.scala
+++ scct/src/test/scala/reaktor/scct/InstrumentationSpec.scala
@@ -47,9 +47,9 @@ trait InstrumentationSupport {
     val settings = new Settings
     val scalaJars = List("scala-compiler.jar", "scala-library.jar")
     val classPath = if (System.getProperty("java.class.path").contains("sbt-launch")) {
-      "./target/scala_2.8.0/classes" :: scalaJars.map("./project/boot/scala-2.8.0/lib/"+_)
+      "./target/scala_2.9.0/classes" :: scalaJars.map("./project/boot/scala-2.9.0/lib/"+_)
     } else {
-      "./out/production/scct" :: scalaJars.map("./project/boot/scala-2.8.0/lib/"+_)
+      "./out/production/scct" :: scalaJars.map("./project/boot/scala-2.9.0/lib/"+_)
     }
     settings.classpath.value = classPath.mkString(":")
     settings
