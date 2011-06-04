56ca7eefd38934bbf4a9028fa1d99620e02dd20c
Mikko Koponen <mikko.koponen@reaktor.fi>
2011-06-04 17:37:34 +0300

    Update to sbt 0.7.7

--- scct/project/plugins/Plugins.scala
+++ scct/project/plugins/Plugins.scala
@@ -3,5 +3,5 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val ideaPluginRepo = "GH-pages repo" at "http://mpeltonen.github.com/maven/"
   val ideaPlugin = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.1-SNAPSHOT"
   // Self-test:
-  // val scctPlugin = "reaktor" % "sbt-scct" % "1.0"
+  // val scctPlugin = "reaktor" % "scct-sbt" % "1.0"
 }
