af9ff7000463cabd5e61a6204b8160ec51a25e93
Konstantin_Grigoriev <Konstantin.V.Grigoriev@gmail.com>
2012-04-04 00:56:02 +0400

    Cobertura xml report generation.

--- scct/src/test/scala/reaktor/scct/report/CoverageDataSpec.scala
+++ scct/src/test/scala/reaktor/scct/report/CoverageDataSpec.scala
@@ -12,6 +12,7 @@ class CoverageDataSpec extends Specification {
       new CoverageData(blocks(true)).percentage mustEqual Some(100)
       new CoverageData(blocks(false)).percentage mustEqual Some(0)
       new CoverageData(blocks(true, true, false, true)).percentage mustEqual Some(75)
+      new CoverageData(blocks(true, true, false)).percentage mustEqual Some(66)
     }
     "skip placeholders" in {
       val data = new CoverageData(CoveredBlock("x", blockName("x"), 1, true) :: blocks(true, true, false, false))
@@ -19,4 +20,17 @@ class CoverageDataSpec extends Specification {
     }
   }
 
+  "Rate calculation" should {
+    "calculate total" in {
+      new CoverageData(blocks()).rate mustEqual None
+      new CoverageData(blocks(true)).rate mustEqual Some(1)
+      new CoverageData(blocks(false)).rate mustEqual Some(0)
+      new CoverageData(blocks(true, true, false, true)).rate mustEqual Some(0.75)
+      new CoverageData(blocks(true, true, false)).rate mustEqual Some(0.66)
+    }
+    "skip placeholders" in {
+      val data = new CoverageData(CoveredBlock("x", blockName("x"), 1, true) :: blocks(true, true, false, false))
+      data.rate mustEqual Some(0.5)
+    }
+  }
 }
