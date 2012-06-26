d8f334cb92efbfa42c68595ae752d24f28d89e3a
Mikko Koponen <mikko.koponen@reaktor.fi>
2012-06-26 03:09:25 +0300

    Tests for plugin opts.

--- src/test/scala/reaktor/scct/ScctInstrumentPluginSpec.scala
+++ src/test/scala/reaktor/scct/ScctInstrumentPluginSpec.scala
@@ -3,6 +3,7 @@ package reaktor.scct
 import org.specs2.mutable._
 import org.specs2.mock._
 import tools.nsc.Global
+import java.io.File
 
 class ScctInstrumentPluginSpec extends Specification with Mockito {
   val sut = new ScctInstrumentPlugin(smartMock[Global])
@@ -18,6 +19,23 @@ class ScctInstrumentPluginSpec extends Specification with Mockito {
       val components = sut.components
       components.size mustEqual 1
       components(0) must haveClass[ScctTransformComponent]
+    }
+  }
+
+  "Plugin options" should {
+    "have defaults" in {
+      sut.options.projectId must not be empty
+      sut.options.baseDir.getName must not be empty
+    }
+    "be settable" in {
+      sut.processOptions(List("basedir:/base/dir", "projectId:myProject"), s => ())
+      sut.options.projectId mustEqual "myProject"
+      sut.options.baseDir.getAbsolutePath mustEqual "/base/dir"
+    }
+    "report error" in {
+      var err = ""
+      sut.processOptions(List("wrong:option"), s => { err = s })
+      err mustEqual "Unknown option: wrong:option"
     }
   }
 }
