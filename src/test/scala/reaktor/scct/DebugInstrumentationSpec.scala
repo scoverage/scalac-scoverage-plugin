b525c4a84bc270d23c0d8e2756f5b121a0eae470
Mikko Koponen <mikko.koponen@reaktor.fi>
2012-08-02 10:31:57 +0300

    Adding commented out testcases for a couple of problem points. Returning to these soon I hope.

--- src/test/scala/reaktor/scct/DebugInstrumentationSpec.scala
+++ src/test/scala/reaktor/scct/DebugInstrumentationSpec.scala
@@ -1,9 +1,46 @@
 package reaktor.scct
 
 class DebugInstrumentationSpec extends InstrumentationSpec {
-  //override def debug = true
+  override def debug = false
 
-  "debug instrumentation" in {
-    classOffsetsMatch("@Some(\"foo\").map(@System.getProperty)")
+  /* works: */
+  "extending java classes" in {
+    offsetsMatch("""|class Foo @extends java.util.ArrayList[String] {
+                    |  override def size = @12
+                    |}""".stripMargin)
   }
+
+  /*
+    Doesn't work, the 'def size' hits this and stops instrumentation:
+      case dd: DefDef if isInAnonymousClass => (false, t)
+  */
+  /*
+  "anonymously extending java classes" in {
+    offsetsMatch("""|class Foo @{
+                    |  val aa = @new java.util.ArrayList[String]() {
+                    |    override def size = {
+                    |      @println("first.")
+                    |      @12
+                    |    }
+                    |  }
+                    |}""".stripMargin)
+
+    }
+   */
+
+  /* This can't work until plugin is moved after uncurry: */
+
+  /*
+  "Literal as def parameter with type () => T" in {
+    offsetsMatch("""|
+                    |final class Full[+A] {
+                    |  def openOr[B >: A](default: => B): B = @default
+                    |}
+                    |
+                    |class Foo @{
+                    |  val x = Full@[Int]()
+                    |  @x.openOr(@0)
+                    |}""".stripMargin)
+  }
+  */
 }
