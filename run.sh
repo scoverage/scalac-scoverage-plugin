431dcfc60f5765a880bb7d09538dcc63397c967e
sksamuel <sam@sksamuel.com>
2013-07-07 14:04:52 +0100

    Added run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+mvn clean install;
+cp target/scales.jar $SCALA_HOME/misc/scala-devel/plugins;
+/home/sam/development/scala-2.10.2/bin/scalac -classpath "$SCALA_HOME/misc/scala-devel/plugins/scales.jar" scratch/Test.scala;
+javap -c Test.class
