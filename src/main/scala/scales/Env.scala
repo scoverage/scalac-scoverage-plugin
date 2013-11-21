6389ceb97f553de0c8f08acd7ab89d75d3e720db
sksamuel <sam@sksamuel.com>
2013-11-21 09:22:27 +0000

    Added system properties for files

--- src/main/scala/scales/Env.scala
+++ src/main/scala/scales/Env.scala
@@ -4,6 +4,6 @@ import java.io.File
 
 /** @author Stephen Samuel */
 object Env {
-  def measurementFile = new File("scales.measurement")
-  def coverageFile = new File("scales.coverage")
+  def measurementFile = new File(Option(System.getProperty("scales.measurement.file")).getOrElse("scales.measurement"))
+  def coverageFile = new File(Option(System.getProperty("scales.coverage.file")).getOrElse("scales.coverage"))
 }
