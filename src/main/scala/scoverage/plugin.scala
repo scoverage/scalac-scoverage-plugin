f144774e8024f9f8ca01207426212388624c862f
Rado Buransky <radoburansky@gmail.com>
2014-01-06 17:55:40 -0800

    Allow exclusion of objects

--- src/main/scala/scoverage/plugin.scala
+++ src/main/scala/scoverage/plugin.scala
@@ -140,12 +140,8 @@ class ScoverageComponent(val global: Global, options: ScoverageOptions)
       }
     }
 
-    def isIncluded(p: PackageDef): Boolean = {
-      new CoverageFilter(options.excludedPackages).isIncluded(p.symbol.fullNameString)
-    }
-
-    def isIncluded(c: ClassDef): Boolean = {
-      new CoverageFilter(options.excludedPackages).isIncluded(c.symbol.fullNameString)
+    def isIncluded(t: Tree): Boolean = {
+      new CoverageFilter(options.excludedPackages).isIncluded(t.symbol.fullNameString)
     }
 
     def className(s: Symbol): String = {
@@ -376,8 +372,12 @@ class ScoverageComponent(val global: Global, options: ScoverageOptions)
 
         // user defined objects
         case m: ModuleDef =>
-          updateLocation(m.symbol)
-          super.transform(tree)
+          if (isIncluded(m)) {
+            updateLocation(m.symbol)
+            super.transform(tree)
+          }
+          else
+            m
 
         /**
          * match with syntax `New(tpt)`.
