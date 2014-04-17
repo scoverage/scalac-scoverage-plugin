70e5b450b70888f21978d2c1eb2cfd4dbc01de8b
sksamuel <sam@sksamuel.com>
2014-04-17 23:44:12 +0100

    Added test for basic macros

--- src/test/scala/scoverage/PluginASTSupportTest.scala
+++ src/test/scala/scoverage/PluginASTSupportTest.scala
@@ -17,10 +17,14 @@ class PluginASTSupportTest
 
   test("scoverage component should handle simple class") {
     compileCodeSnippet( """ class Test { val name = "sammy" } """)
+    assert(!reporter.hasErrors)
   }
 
-  test("scoverage component should handle macro") {
-    compileCodeSnippet( """ object MyMacro {
+  test("scoverage component should handle basic macros") {
+    compileCodeSnippet( """ import scala.language.experimental.macros
+                          | import scala.reflect.macros.Context
+                          |
+                          | object MyMacro {
                           |  def test = macro testImpl
                           |  def testImpl(c: Context): c.Expr[Unit] = {
                           |    import c.universe._
@@ -28,6 +32,9 @@ class PluginASTSupportTest
                           |      println("macro test")
                           |    }
                           |  }
-                          |} """)
+                          |} """.stripMargin)
+    assert(!reporter.hasErrors)
   }
 }
+
+
