5985930c322ae51ff5528e67cf0d0cdea2de536d
sksamuel <sam@sksamuel.com>
2014-04-17 23:38:52 +0100

    Added support for capturing error output of reproter

--- src/test/scala/scoverage/PluginASTSupportTest.scala
+++ src/test/scala/scoverage/PluginASTSupportTest.scala
@@ -1,27 +1,33 @@
 package scoverage
 
 import org.scalatest.mock.MockitoSugar
-import org.scalatest.{FunSuite, OneInstancePerTest}
+import org.scalatest._
 
 /** @author Stephen Samuel */
-class PluginASTSupportTest extends FunSuite with MockitoSugar with OneInstancePerTest with PluginSupport {
+class PluginASTSupportTest
+  extends FunSuite
+  with MockitoSugar
+  with OneInstancePerTest
+  with PluginSupport
+  with BeforeAndAfterEachTestData {
+
+  override protected def afterEach(testData: TestData): Unit = {
+    assert(!reporter.hasErrors)
+  }
 
   test("scoverage component should handle simple class") {
     compileCodeSnippet( """ class Test { val name = "sammy" } """)
   }
 
   test("scoverage component should handle macro") {
-    compileCodeSnippet( """ class Test { val name = "sammy" } """)
+    compileCodeSnippet( """ object MyMacro {
+                          |  def test = macro testImpl
+                          |  def testImpl(c: Context): c.Expr[Unit] = {
+                          |    import c.universe._
+                          |    reify {
+                          |      println("macro test")
+                          |    }
+                          |  }
+                          |} """)
   }
 }
-
-//
-//object MyMacro {
-//  def test = macro testImpl
-//  def testImpl(c: Context): c.Expr[Unit] = {
-//    import c.universe._
-//    reify {
-//      println("macro test")
-//    }
-//  }
-//}
--- src/test/scala/scoverage/PluginRunner.scala
+++ src/test/scala/scoverage/PluginRunner.scala
@@ -1,12 +1,20 @@
 package scoverage
 
 import java.io.{FileNotFoundException, File}
+import scala.tools.nsc.reporters.Reporter
 
 /** @author Stephen Samuel */
 trait PluginSupport {
 
   val scalaVersion = "2.10.3"
   val shortScalaVersion = "2.10"
+
+  val settings = new scala.tools.nsc.Settings
+  val classPath = getScalaJars.map(_.getAbsolutePath) :+ sbtCompileDir.getAbsolutePath
+  settings.Xprint.value = List("all")
+  settings.classpath.value = classPath.mkString(":")
+
+  val reporter = new scala.tools.nsc.reporters.ConsoleReporter(settings)
 
   def writeCodeSnippetToTempFile(code: String): File = {
     val file = File.createTempFile("scoverage_snippet", ".scala")
@@ -17,8 +25,7 @@ trait PluginSupport {
   def compileCodeSnippet(code: String): SimpleCompiler = compileSourceFiles(writeCodeSnippetToTempFile(code))
 
   def compileSourceFiles(files: File*): SimpleCompiler = {
-    val settings = createSettings
-    val compiler = new SimpleCompiler(settings)
+    val compiler = new SimpleCompiler(settings, reporter)
     val command = new scala.tools.nsc.CompilerCommand(files.map(_.getAbsolutePath).toList, settings)
     new compiler.Run().compile(command.files)
     compiler
@@ -43,17 +50,10 @@ trait PluginSupport {
     else throw new FileNotFoundException(s"Could not locate SBT compile directory for plugin files [$dir]")
   }
 
-  def createSettings = {
-    val settings = new scala.tools.nsc.Settings
-    val classPath = getScalaJars.map(_.getAbsolutePath) :+ sbtCompileDir.getAbsolutePath
-    settings.Xprint.value = List("all")
-    settings.classpath.value = classPath.mkString(":")
-    settings
-  }
 }
 
-class SimpleCompiler(settings: scala.tools.nsc.Settings)
-  extends scala.tools.nsc.Global(settings, new scala.tools.nsc.reporters.ConsoleReporter(settings)) {
+class SimpleCompiler(settings: scala.tools.nsc.Settings, reporter: scala.tools.nsc.reporters.Reporter)
+  extends scala.tools.nsc.Global(settings, reporter) {
   val scoverageComponent = new ScoverageComponent(this)
   scoverageComponent.setOptions(new ScoverageOptions())
   override def computeInternalPhases() {
