cce2890e4d38f7c19df65ef5b4cc6e502bb9d9dd
sksamuel <sam@sksamuel.com>
2014-11-20 21:11:57 +0000

    Updated XML writer to include top level class

--- scalac-scoverage-plugin/src/main/scala/scoverage/report/ScoverageXmlWriter.scala
+++ scalac-scoverage-plugin/src/main/scala/scoverage/report/ScoverageXmlWriter.scala
@@ -17,67 +17,6 @@ class ScoverageXmlWriter(sourceDir: File, outputDir: File, debug: Boolean) {
     IOUtils.writeToFile(file, new PrettyPrinter(120, 4).format(xml(coverage)))
   }
 
-  def statement(stmt: Statement): Node = {
-    debug match {
-      case true =>
-        <statement package={stmt.location.packageName}
-                   class={stmt.location.className}
-                   method={stmt.location.method}
-                   start={stmt.start.toString}
-                   line={stmt.line.toString}
-                   symbol={Serializer.escape(stmt.symbolName)}
-                   tree={Serializer.escape(stmt.treeName)}
-                   branch={stmt.branch.toString}
-                   invocation-count={stmt.count.toString}>
-          {Serializer.escape(stmt.desc)}
-        </statement>
-      case false =>
-          <statement package={stmt.location.packageName}
-                     class={stmt.location.className}
-                     method={stmt.location.method}
-                     start={stmt.start.toString}
-                     line={stmt.line.toString}
-                     branch={stmt.branch.toString}
-                     invocation-count={stmt.count.toString}/>
-    }
-  }
-
-  def method(method: MeasuredMethod): Node = {
-    <method name={method.name}
-            statement-count={method.statementCount.toString}
-            statements-invoked={method.invokedStatementCount.toString}
-            statement-rate={method.statementCoverageFormatted}
-            branch-rate={method.branchCoverageFormatted}>
-      <statements>
-        {method.statements.map(statement)}
-      </statements>
-    </method>
-  }
-
-  def klass(klass: MeasuredClass): Node = {
-    <class name={klass.name}
-           filename={klass.source.replace(sourceDir.getAbsolutePath, "")}
-           statement-count={klass.statementCount.toString}
-           statements-invoked={klass.invokedStatementCount.toString}
-           statement-rate={klass.statementCoverageFormatted}
-           branch-rate={klass.branchCoverageFormatted}>
-      <methods>
-        {klass.methods.map(method)}
-      </methods>
-    </class>
-  }
-
-  def pack(pack: MeasuredPackage): Node = {
-    <package name={pack.name}
-             statement-count={pack.statementCount.toString}
-             statements-invoked={pack.invokedStatementCount.toString}
-             statement-rate={pack.statementCoverageFormatted}>
-      <classes>
-        {pack.classes.map(klass)}
-      </classes>
-    </package>
-  }
-
   def xml(coverage: Coverage): Node = {
     <scoverage statement-count={coverage.statementCount.toString}
                statements-invoked={coverage.invokedStatementCount.toString}
@@ -90,5 +29,69 @@ class ScoverageXmlWriter(sourceDir: File, outputDir: File, debug: Boolean) {
       </packages>
     </scoverage>
   }
+
+  private def statement(stmt: Statement): Node = {
+    debug match {
+      case true =>
+        <statement package={stmt.location.packageName}
+                   class={stmt.location.className}
+                   topLevelClass={stmt.location.topLevelClass}
+                   method={stmt.location.method}
+                   start={stmt.start.toString}
+                   line={stmt.line.toString}
+                   symbol={Serializer.escape(stmt.symbolName)}
+                   tree={Serializer.escape(stmt.treeName)}
+                   branch={stmt.branch.toString}
+                   invocation-count={stmt.count.toString}>
+          {Serializer.escape(stmt.desc)}
+        </statement>
+      case false =>
+          <statement package={stmt.location.packageName}
+                     class={stmt.location.className}
+                     topLevelClass={stmt.location.topLevelClass}
+                     method={stmt.location.method}
+                     start={stmt.start.toString}
+                     line={stmt.line.toString}
+                     branch={stmt.branch.toString}
+                     invocation-count={stmt.count.toString}/>
+    }
+  }
+
+  private def method(method: MeasuredMethod): Node = {
+    <method name={method.name}
+            statement-count={method.statementCount.toString}
+            statements-invoked={method.invokedStatementCount.toString}
+            statement-rate={method.statementCoverageFormatted}
+            branch-rate={method.branchCoverageFormatted}>
+      <statements>
+        {method.statements.map(statement)}
+      </statements>
+    </method>
+  }
+
+  private def klass(klass: MeasuredClass): Node = {
+    <class name={klass.name}
+           filename={klass.source.replace(sourceDir.getAbsolutePath, "")}
+           statement-count={klass.statementCount.toString}
+           statements-invoked={klass.invokedStatementCount.toString}
+           statement-rate={klass.statementCoverageFormatted}
+           branch-rate={klass.branchCoverageFormatted}>
+      <methods>
+        {klass.methods.map(method)}
+      </methods>
+    </class>
+  }
+
+  private def pack(pack: MeasuredPackage): Node = {
+    <package name={pack.name}
+             statement-count={pack.statementCount.toString}
+             statements-invoked={pack.invokedStatementCount.toString}
+             statement-rate={pack.statementCoverageFormatted}>
+      <classes>
+        {pack.classes.map(klass)}
+      </classes>
+    </package>
+  }
+
 }
 
