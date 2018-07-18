0677bbe914e9b02595bfc935827f18d9254d7b4a
JanBessai <jan.bessai@tu-dortmund.de>
2018-07-18 15:23:16 +0200

    Quick fix to get new Scala collections compatibility in test.

--- scalac-scoverage-plugin/src/test/scala/scoverage/PluginCoverageTest.scala
+++ scalac-scoverage-plugin/src/test/scala/scoverage/PluginCoverageTest.scala
@@ -263,9 +263,9 @@ class PluginCoverageTest
                                    |    }
                                    |  }""".stripMargin)
     assert(!compiler.reporter.hasErrors)
-    // 2 statements for the two applies in Seq, one for each literal which is 6, one for the flat map,
-    // one for the map, one for the yield op.
-    compiler.assertNMeasuredStatements(11)
+    // 2 statements for the two applies in Seq, one for each literal which is 6, one for the operation passed to yield.
+    // Depending on the collections api version, there can be additional implicit canBuildFrom statements.
+    compiler.assertAtLeastNStatements(9)
   }
 
   test("plugin should not instrument local macro implementation") {
