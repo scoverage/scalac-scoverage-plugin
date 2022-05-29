d262151c4ca0da1f3c25629102744a6b83b94117
Chris Kipp <ckipp@pm.me>
2022-05-29 09:50:29 +0200

    deps: bump scalafmt to 3.5.7
    Also make sure new jsdomtest modules has the correct scalacoptions for
    styleFix to run.

--- plugin/src/test/scala/scoverage/PluginASTSupportTest.scala
+++ plugin/src/test/scala/scoverage/PluginASTSupportTest.scala
@@ -93,31 +93,33 @@ class PluginASTSupportTest extends FunSuite with MacroSupport {
 
   test("scoverage component should ignore complex macros #11") {
     val compiler = ScoverageCompiler.default
-    compiler.compileCodeSnippet(s""" object ComplexMacro {
-                                   |
-                                   |  import scala.language.experimental.macros
-                                   |  import ${macroContextPackageName}.Context
-                                   |
-                                   |  def debug(params: Any*): Unit = macro debugImpl
-                                   |
-                                   |  def debugImpl(c: Context)(params: c.Expr[Any]*) = {
-                                   |    import c.universe._
-                                   |
-                                   |    val trees = params map {param => (param.tree match {
-                                   |      case Literal(Constant(_)) => reify { print(param.splice) }
-                                   |      case _ => reify {
-                                   |        val variable = c.Expr[String](Literal(Constant(show(param.tree)))).splice
-                                   |        print(s"$$variable = $${param.splice}")
-                                   |      }
-                                   |    }).tree
-                                   |    }
-                                   |
-                                   |    val separators = (1 until trees.size).map(_ => (reify { print(", ") }).tree) :+ (reify { println() }).tree
-                                   |    val treesWithSeparators = trees zip separators flatMap {p => List(p._1, p._2)}
-                                   |
-                                   |    c.Expr[Unit](Block(treesWithSeparators.toList, Literal(Constant(()))))
-                                   |  }
-                                   |} """.stripMargin)
+    compiler.compileCodeSnippet(
+      s""" object ComplexMacro {
+         |
+         |  import scala.language.experimental.macros
+         |  import ${macroContextPackageName}.Context
+         |
+         |  def debug(params: Any*): Unit = macro debugImpl
+         |
+         |  def debugImpl(c: Context)(params: c.Expr[Any]*) = {
+         |    import c.universe._
+         |
+         |    val trees = params map {param => (param.tree match {
+         |      case Literal(Constant(_)) => reify { print(param.splice) }
+         |      case _ => reify {
+         |        val variable = c.Expr[String](Literal(Constant(show(param.tree)))).splice
+         |        print(s"$$variable = $${param.splice}")
+         |      }
+         |    }).tree
+         |    }
+         |
+         |    val separators = (1 until trees.size).map(_ => (reify { print(", ") }).tree) :+ (reify { println() }).tree
+         |    val treesWithSeparators = trees zip separators flatMap {p => List(p._1, p._2)}
+         |
+         |    c.Expr[Unit](Block(treesWithSeparators.toList, Literal(Constant(()))))
+         |  }
+         |} """.stripMargin
+    )
     assert(!compiler.reporter.hasErrors)
   }
 
