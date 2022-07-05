94f04b5fe74696ada24d6882a1a002686711f629
Roland Tritsch <roland@tritsch.email>
2022-07-05 09:11:07 +0100

    Running scalafmt

--- plugin/src/main/scala/scoverage/CoverageFilter.scala
+++ plugin/src/main/scala/scoverage/CoverageFilter.scala
@@ -32,9 +32,12 @@ class RegexCoverageFilter(
     excludedSymbols: Seq[String],
     reporter: Reporter
 ) extends CoverageFilter {
-  if (excludedPackages.nonEmpty) reporter.echo(s"scoverage excludedPackages: ${excludedPackages}")
-  if (excludedFiles.nonEmpty) reporter.echo(s"scoverage excludedFiles: ${excludedFiles}")
-  if (excludedSymbols.nonEmpty) reporter.echo(s"scoverage excludedSymbols: ${excludedSymbols}")
+  if (excludedPackages.nonEmpty)
+    reporter.echo(s"scoverage excludedPackages: ${excludedPackages}")
+  if (excludedFiles.nonEmpty)
+    reporter.echo(s"scoverage excludedFiles: ${excludedFiles}")
+  if (excludedSymbols.nonEmpty)
+    reporter.echo(s"scoverage excludedSymbols: ${excludedSymbols}")
 
   val excludedClassNamePatterns = excludedPackages.map(_.r.pattern)
   val excludedFilePatterns = excludedFiles.map(_.r.pattern)
