423090caf567768912263bed17f9330cb17f6cd4
Chris Kipp <ckipp@pm.me>
2022-10-09 12:28:58 +0200

    chore: add support for 2.13.10
    Closes #506

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -25,6 +25,7 @@ coursier fetch \
   org.scoverage:scalac-scoverage-plugin_2.13.7:$version \
   org.scoverage:scalac-scoverage-plugin_2.13.8:$version \
   org.scoverage:scalac-scoverage-plugin_2.13.9:$version \
+  org.scoverage:scalac-scoverage-plugin_2.13.10:$version \
   org.scoverage:scalac-scoverage-runtime_2.12:$version \
   org.scoverage:scalac-scoverage-runtime_2.13:$version \
   org.scoverage:scalac-scoverage-runtime_sjs1_2.12:$version \
