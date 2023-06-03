5519b6ea63ec399750d2975d0e555d28c062db13
Chris Kipp <ckipp@pm.me>
2023-06-03 10:00:40 +0200

    chore: drop support for * < 2.13.5 for 2.13
    Mainly because semanticdb has started dropping support as well.

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -12,11 +12,6 @@ coursier fetch \
   org.scoverage:scalac-scoverage-plugin_2.12.15:$version \
   org.scoverage:scalac-scoverage-plugin_2.12.16:$version \
   org.scoverage:scalac-scoverage-plugin_2.12.17:$version \
-  org.scoverage:scalac-scoverage-plugin_2.13.0:$version \
-  org.scoverage:scalac-scoverage-plugin_2.13.1:$version \
-  org.scoverage:scalac-scoverage-plugin_2.13.2:$version \
-  org.scoverage:scalac-scoverage-plugin_2.13.3:$version \
-  org.scoverage:scalac-scoverage-plugin_2.13.4:$version \
   org.scoverage:scalac-scoverage-plugin_2.13.5:$version \
   org.scoverage:scalac-scoverage-plugin_2.13.5:$version \
   org.scoverage:scalac-scoverage-plugin_2.13.6:$version \
@@ -24,6 +19,7 @@ coursier fetch \
   org.scoverage:scalac-scoverage-plugin_2.13.8:$version \
   org.scoverage:scalac-scoverage-plugin_2.13.9:$version \
   org.scoverage:scalac-scoverage-plugin_2.13.10:$version \
+  org.scoverage:scalac-scoverage-plugin_2.13.11:$version \
   org.scoverage:scalac-scoverage-runtime_2.12:$version \
   org.scoverage:scalac-scoverage-runtime_2.13:$version \
   org.scoverage:scalac-scoverage-runtime_sjs1_2.12:$version \
